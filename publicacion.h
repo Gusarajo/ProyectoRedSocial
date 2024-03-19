6d04ccfb2e5f6b72962877e841a7261cbdf43a30
gabriel250206 <19050@portalcedi.edu.mx>
2024-03-19 13:45:46 -0600

    Publicación

--- publicacion.h
+++ publicacion.h
@@ -6,6 +6,17 @@ class Publicaciones
 int id;
 public:
 string fecha,contenido;
+usuario* ptrusuario;
+mostrarPublicacion()
+{
+    cout<<"Fecha: "<< fecha<< " \n contenido: "<< contenido << " \n usuario: "<< usuario<<endl;
+}
+    Publicaciones(Usuario* usuario, string fecha, string contenido)
+    {
+        this-> ptrusuario = usuario;
+        this-> fecha=fecha;
+        this-> contenido=contenido;
+    }
 };
 
 
