6b656f4e55d291692eb0c715e0aad02d0cc3e8af
Diogodtilinkawai <10143@portalcedi.edu.mx>
2024-04-02 12:50:02 -0600

    publicacion mas usuario completo

--- Usuario.h
+++ Usuario.h
@@ -1,7 +1,79 @@
 #ifndef usuario_h
 #define usuario_h
-#include <iostream>
-#include <string>
 
+class Usuario
+{
+int id;
+piublic: 
+string nombre;
+int edad;
+string nacionalidad;
+vector <*usuarios> amigos;
+vector <*publicacion>publicaciones;
+int getid()
+{return this->id;}
+void mostrar()
+{
+    cout<< "id: "<< getid()<<"\n nombre: "<< this->nombre<< "\n edad:"<< this-> edad<< "\n nacionalidad: "<< this->edad<<endl;
+}
+void mostrarAmigos()
+{
+    for (int i=0;i<this->amigos.size();i++)
+    {
+        cout<< i <<":"<<  amigos[i].mostrar()<<endl;
+    }
+}
+void mostrarPublicaciones()
+{
+    for (int i=0;i<this->publicaciones.size();i++)
+    {
+        cout << i << ":"<< Publicaciones[i].mostrarPublicacion()<<endl;
+    }
+
+}
+void agregarAmigo(Usuario* nuevoAmigo)
+{
+    this->amigos.push_back(nuevoAmigo);
+    nuevoAmigo.amigos.push_back(this);
+}
+void crearPublicacion()
+{
+    Publicacion newPub;
+    cout<< "Estas creando una publicacion"<<endl;
+    newPub.ptrusuario=this;
+    cout << "cual es la fecha \n";
+    cin >> newPub.fecha;
+    cout << "cual es el contenido de la publicacion (sin espacios)\n"
+    cin >> newPub.contenido;
+    this ->publicaciones.push_back(newPub);
+}
+Usuario* getAmigo(int id)
+{
+ for(int i=0;i<this->amigos.size();i++)
+ {
+    if(this->amigos[i].id==id)
+    {
+        return amigos[i];
+    }
+ }
+ cout << "no existe un amigo con ese id \n";
+ return nullptr;
+}
+Usuario(string nombre)
+{
+    this->nombre=nombre;
+}
+Usuario(string nombre, int edad)
+{
+    this->nombre=nombre;
+    this->edad=edad;
+}
+Usuario(string nombre, int edad, string nacionalidad)
+{
+    this->nombre=nombre;
+    this->edad=edad;
+    this-> nacionalidad=nacionalidad;
+}
+};
 
 #endif 
--- publicacion.h
+++ publicacion.h
@@ -1,32 +1,21 @@
 #ifndef publicacion_h
 #define publicacion_h
 #include "Usuario.h" 
-class Publicaciones
+class Publicacion
 {
 int id;
 public:
 string fecha,contenido;
-usuario* ptrusuario;
+Usuario* ptrusuario;
 mostrarPublicacion()
 {
     cout<<"Fecha: "<< fecha<< " \n contenido: "<< contenido << " \n usuario: "<< usuario<<endl;
 }
-    Publicaciones(Usuario* usuario, string fecha, string contenido)
+Publicacion(Usuario* usuario, string fecha, string contenido)
     {
         this-> ptrusuario = usuario;
         this-> fecha=fecha;
         this-> contenido=contenido;
     }
-  Publicaciones(usuario* usuario,string fecha, string contenido)
-  {
-    this->fecha=fecha;
-    this->ptrusuario=usuario;
-    this ->contenido=contenido;
-  }
 };
-
-
-
-
-
 #endif
